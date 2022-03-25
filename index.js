42a6dd5eb7de2d6bd5c22dcc4bf492ce19bf804d
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-25 16:25:23 +0700

    "test"

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
+
 //import handlebar
 const hbs = require("express-handlebars");
+
 const restaurantsRouter = require("./routes/restaurants");
 const app = express();
+
 //Import middleware
 const logger = require("./middleware/logger");
-//Setup Template engines
 
+//Setup Template engines
 app.set("view engine", "hbs");
 app.engine("html", require("hbs").__express);
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 //Custom middleware
 app.use(logger);
+
 // Routes
 app.use("/apis/restaurants", restaurantsRouter);
 
